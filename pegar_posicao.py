367bf8b9910bb6ccd7fccb71d55a80defa91cca2
Anderson Samartin <andersonsamartin@protonmail.com>
2024-10-19 14:28:11 -0300

    pegar_posicao.py
     Programa Auxiliar.

---
+++ pegar_posicao.py
@@ -0,0 +1,9 @@
+import time
+import pyautogui
+
+time.sleep(5)
+print(pyautogui.position())
+
+pyautogui.scroll(200)
+
+# O código funciona da seguinte maneira: Execute-o no seu editor de código e posicione o mouse na posição desejada em até 5 segundos. Após esses 5 segundos o script irá capturar a posição do seu mouse e exibí-la no TERMINAL.
